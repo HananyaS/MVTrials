0e673f00b9ddc5d8e5ac7e383e7f3202c4e94df6
HananyaS <shacharhananya@gmail.com>
2023-07-09 10:08:12 +0300

    added create_grid_commands file

---
+++ reg_model/create_grid_commands.py
@@ -0,0 +1,10 @@
+# datasets = ["Iris", "Accent", "Ecoli"]
+datasets = ["Breast", "Sonar"]
+commands_file = "grid_search_commands"
+
+with open(commands_file, "w") as f:
+    for dataset in datasets:
+        for i in range(7):
+            f.write(
+                f"python main_experimens.py --run_grid True --dataset {dataset} --resfile grid_search_results/{dataset.lower()}_grid_val.csv --run_ts False --run_grid True\n"
+            )
